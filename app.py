0b4727c1034bbab48909d4236af15b16e3587b5d
Brilliant ZC <2411464044@qq.com>
2021-04-14 09:31:28 +0800

    添加爬取下层网页爬虫，完善工作数据和分析图表

--- app.py
+++ app.py
@@ -46,18 +46,110 @@ def showjob_detail_then():
 
 @app.route('/job_pic')
 def showjob_pic():
-    job_classify = [] # 评分
-    num = []   # 评分的电影数
+    job_classify = []
+    num = []
+    education_dic = {}
+    campany = []
+    cnum = []
     conn = sqlite3.connect("job.db")
     cur = conn.cursor()
-    sql = "select job_name,count(job_name) from jobinfo "
-    data = cur.execute(sql)
+    sql1 = "select count(job_name) from jobinfo where job_name like '%Java%'"
+    sql2 = "select count(job_name) from jobinfo where job_name like '%Python%'"
+    sql3 = "select count(job_name) from jobinfo where job_name like '%NET%' "
+    sql4 = "select count(job_name) from jobinfo where job_name like '%Android%' "
+    sql5 = "select count(job_name) from jobinfo where job_name like '%C++%' "
+    sql6 = "select count(job_name) from jobinfo where job_name like '%C语言%' "
+    sql7 = "select count(job_name) from jobinfo where job_name like '%C#%' "
+    sql8 = "select count(job_name) from jobinfo where job_name like '%PHP%' "
+    sql9 = "select count(job_education) from jobinfo where job_education like '%硕士%'"
+    sql10 = "select count(job_education) from jobinfo where job_education like '%本科%'"
+    sql11 = "select count(job_education) from jobinfo where job_education like '%大专%'"
+    sql12 = "select count(job_education) from jobinfo where job_education like '%中专%'"
+    sql13 = "select count(job_education) from jobinfo where job_education like '%高中%'"
+    sql14 = "select count(job_education) from jobinfo where job_education like '%初中%'"
+    sql15 = "select count(job_catacom) from jobinfo where job_catacom like '%上市公司%'"
+    sql16 = "select count(job_catacom) from jobinfo where job_catacom like '%民营公司%'"
+    sql17 = "select count(job_catacom) from jobinfo where job_catacom like '%合资%'"
+    sql18 = "select count(job_catacom) from jobinfo where job_catacom like '%国企%'"
+    sql19 = "select count(job_catacom) from jobinfo where job_catacom like '%外资%'"
+    sql20 = "select count(job_catacom) from jobinfo where job_catacom like '%创业公司%'"
+    data = cur.execute(sql1)
     for item in data:
-        score.append(item[0])
-        num.append(item[1])
+        job_classify.append('Java')
+        num.append(item[0])
+    data = cur.execute(sql2)
+    for item in data:
+        job_classify.append('Python')
+        num.append(item[0])
+    data = cur.execute(sql3)
+    for item in data:
+        job_classify.append('.NET')
+        num.append(item[0])
+    data = cur.execute(sql4)
+    for item in data:
+        job_classify.append('Android')
+        num.append(item[0])
+    data = cur.execute(sql5)
+    for item in data:
+        job_classify.append('C++')
+        num.append(item[0])
+    data = cur.execute(sql6)
+    for item in data:
+        job_classify.append('C')
+        num.append(item[0])
+    data = cur.execute(sql7)
+    for item in data:
+        job_classify.append('C#')
+        num.append(item[0])
+    data = cur.execute(sql8)
+    for item in data:
+        job_classify.append('PHP')
+        num.append(item[0])
+    data = cur.execute(sql9)
+    for item in data:
+        education_dic['硕士'] = item[0]
+    data = cur.execute(sql10)
+    for item in data:
+        education_dic['本科'] = item[0]
+    data = cur.execute(sql11)
+    for item in data:
+        education_dic['大专'] = item[0]
+    data = cur.execute(sql12)
+    for item in data:
+        education_dic['中专'] = item[0]
+    data = cur.execute(sql13)
+    for item in data:
+        education_dic['高中'] = item[0]
+    data = cur.execute(sql14)
+    for item in data:
+        education_dic['初中'] = item[0]
+    data = cur.execute(sql15)
+    for item in data:
+        campany.append('上市公司')
+        cnum.append(item[0])
+    data = cur.execute(sql16)
+    for item in data:
+        campany.append('民营公司')
+        cnum.append(item[0])
+    data = cur.execute(sql17)
+    for item in data:
+        campany.append('合资')
+        cnum.append(item[0])
+    data = cur.execute(sql18)
+    for item in data:
+        campany.append('国企')
+        cnum.append(item[0])
+    data = cur.execute(sql19)
+    for item in data:
+        campany.append('外资')
+        cnum.append(item[0])
+    data = cur.execute(sql20)
+    for item in data:
+        campany.append('创业公司')
+        cnum.append(item[0])
     cur.close()
     conn.close()
-    return render_template("job_pic.html", score=score, num=num)
+    return render_template("job_pic.html", job_classify=job_classify, num=num,education_dic=education_dic,campany=campany,cnum=cnum)
 
 @app.route('/word')
 def showword():
---
+++ spider-for-detail.py
@@ -0,0 +1,86 @@
+# -*- coding = utf-8 -*-
+# @Time : 2021/4/13 21:20
+# @Author : brilliantZC
+# @File : spider-for-detail.py
+# @Software : PyCharm
+
+# 用于爬取职位的详细信息，爬5k条用于制作词云
+
+from selenium.webdriver import Chrome
+from selenium.common.exceptions import NoSuchElementException
+import time
+import sqlite3  #进行SQLite数据库操作
+
+def getData(url):
+    web = Chrome()
+    web.get(url)
+    datalist = []
+    for i in range(2,50):
+        time.sleep(2)
+        data_list = web.find_elements_by_xpath('/html/body/div[2]/div[3]/div/div[2]/div[4]/div[1]/div')
+        for line in data_list:
+            data = []
+            line.find_element_by_xpath('./a/p[1]').click()
+            web.switch_to.window(web.window_handles[-1])
+            try:
+                job_detail = web.find_element_by_xpath('/html/body/div[3]/div[2]/div[3]/div[1]/div').text.strip().replace('\n', '').replace('\r', '')\
+                    .replace('工作职责:', '').replace('专业要求:', '').replace('岗位职责：','').replace('综合素质:','').replace('"', '').replace('“','')\
+                    .replace('1.','').replace('1、','').replace('2.','').replace('2、','').replace('3.','').replace('3、','').replace('4.','').replace('4、','').replace('5.','').replace('5、','')\
+                    .replace('6.','').replace('6、','').replace('7.', '').replace('7、', '').replace('8.','').replace('8、','').replace('9.','').replace('9、','')
+            except NoSuchElementException as e:
+                job_detail = ""
+            data.append(job_detail)
+            datalist.append(data)
+            # 关掉子窗口
+            web.close()
+            # 回到原来的窗口中
+            web.switch_to.window(web.window_handles[0])
+
+        web.find_element_by_xpath('//*[@id="jump_page"]').clear()
+        web.find_element_by_xpath('//*[@id="jump_page"]').send_keys(i)
+        web.find_element_by_xpath('/html/body/div[2]/div[3]/div/div[2]/div[4]/div[2]/div/div/div/span[4]').click()
+    return datalist
+
+
+def saveDataDB(datalist,dbpath):
+    init_db(dbpath)
+    conn = sqlite3.connect(dbpath)
+    cur = conn.cursor()
+
+    for data in datalist:
+        for index in range(len(data)):
+            data[index] = '"'+data[index]+'"'
+        sql = '''
+            insert into jobdetail(
+            job_detail)
+            values(%s)''' % ",".join(data)
+        cur.execute(sql)
+        conn.commit()
+    cur.close()
+    conn.close()
+
+def init_db(dbpath):
+    sql = '''
+        create table jobdetail
+        (
+        id integer primary key autoincrement ,
+        job_detail text
+        )
+    '''  # 创建数据表
+    conn = sqlite3.connect(dbpath)
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    url = 'https://search.51job.com/list/150200,000000,0000,01,9,99,+,2,1.html?lang=c&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&ord_field=0&dibiaoid=0&line=&welfare='
+    datalist = getData(url)
+    dbpath = "jobDetail.db"
+    saveDataDB(datalist, dbpath)
+
+
+
+
+
+
